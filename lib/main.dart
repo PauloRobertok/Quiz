c41c67d85166154162db1d73dc23fbe0624a6772
Paulo Roberto <paulora999@gmail.com>
2023-06-20 21:55:47 -0300

    Criando a tela de resultados

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,49 @@ import 'homepage.dart';
 import 'quiz.dart';
 
 void main() {
-  runApp(const Quiz());
+  runApp(const Resultado(acertos: 3,));
+}
+
+class Resultado extends StatelessWidget {
+  const Resultado({super.key, required this.acertos});
+  final int acertos;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('Quiz!')),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  Text(
+                    'Resultado',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    'Você acertou:\n$acertos de 10 perguntas',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Pressionado');
+                      },
+                      child: Text(
+                        'Jogar Novamente',
+                        style: TextStyle(fontSize: 31),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.fromLTRB(55, 15, 55, 15)),
+                    ),
+                  ),
+                ])),
+          )),
+    );
+  }
 }
